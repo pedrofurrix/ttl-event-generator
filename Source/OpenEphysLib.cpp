be4703378d2fa395de28981379b631cf4c14537e
Pedro Félix <127769926+pedrofurrix@users.noreply.github.com>
2023-09-27 12:02:09 +0100

    Update OpenEphysLib.cpp

--- Source/OpenEphysLib.cpp
+++ Source/OpenEphysLib.cpp
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <PluginInfo.h>
 
-#include "ProcessorPlugin.h"
+#include "TTLEventGenerator.h"
 #include <string>
 
 #ifdef WIN32
